54ace0c99d399e23b3fdd552fa859d00598134b4
Jason Falk <jasonfalk9@gmail.com>
2023-08-24 12:08:38 -0700

    Add new api call for drawing full deckOfCards.

--- src/js/DeckOfCardsNewDeck.js
+++ src/js/DeckOfCardsNewDeck.js
@@ -1,15 +1,29 @@
 export default class DeckOfCardsNewDeck {
-  static async NewDeckApiCall() {
+  static async newDeckApiCall() {
     try {
-      const response = await fetch(`https://deckofcardsapi.com/api/deck/new/`)
-      const jsonifiedResponse = await response.json();
+      const response = await fetch(`https://deckofcardsapi.com/api/deck/new/shuffle/`);
+      const jsonifiedNewDeckResponse = await response.json();
       if (!response.ok) {
         const errorMessage = `${response.status} ${response.statusText}`;
         throw new Error(errorMessage);
       }
-      return jsonifiedResponse;
+      return jsonifiedNewDeckResponse;
     } catch(error) {
-      return error
+      return error;
     }
-  } 
+  }
+
+  static async drawFullDeckOfCards(deckId) {
+    try {
+      const response = await fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=52`);
+      const jsonifiedDrawCardResponse = await response.json();
+      if (!response.ok) {
+        const errorMessage = `${response.status} ${response.statusText}`;
+        throw new Error(errorMessage);
+      }
+      return jsonifiedDrawCardResponse;
+    } catch(error) {
+      return error;
+    }
+  }
 }
