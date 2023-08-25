b34601dc7de69ee2d06f301c71f68950c518c0ed
Jason Falk <jasonfalk9@gmail.com>
2023-08-25 15:46:07 -0700

    Revise api call for 2 decks and 104 drawn cards.

--- src/js/DeckOfCardsApi.js
+++ src/js/DeckOfCardsApi.js
@@ -1,7 +1,7 @@
 export default class DeckOfCardsApi {
   static async newDeckApiCall() {
     try {
-      const response = await fetch(`https://deckofcardsapi.com/api/deck/new/shuffle/`);
+      const response = await fetch(`https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=2`);
       const jsonifiedNewDeckResponse = await response.json();
       if (!response.ok) {
         const errorMessage = `${response.status} ${response.statusText}`;
@@ -15,7 +15,7 @@ export default class DeckOfCardsApi {
 
   static async drawFullDeckOfCards(deckId) {
     try {
-      const response = await fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=52`);
+      const response = await fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=104`);
       const jsonifiedDrawCardResponse = await response.json();
       if (!response.ok) {
         const errorMessage = `${response.status} ${response.statusText}`;
