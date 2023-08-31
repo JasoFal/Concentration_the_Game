e7b270d53ed6b1366988b9d54b03c7a90564af75
Jason Falk <jasonfalk9@gmail.com>
2023-08-31 09:33:55 -0700

    Set the number of cards to 32.

--- src/js/DeckOfCardsApi.js
+++ src/js/DeckOfCardsApi.js
@@ -1,7 +1,7 @@
 export default class DeckOfCardsApi {
   static async newDeckApiCall() {
     try {
-      const response = await fetch(`https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=2`);
+      const response = await fetch(`https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=2&cards=AS,AC,AD,AH,KS,KC,KD,KH,QS,QC,QD,QH,JS,JC,JD,JH`);
       const jsonifiedNewDeckResponse = await response.json();
       if (!response.ok) {
         const errorMessage = `${response.status} ${response.statusText}`;
@@ -15,7 +15,7 @@ export default class DeckOfCardsApi {
 
   static async drawFullDeckOfCards(deckId) {
     try {
-      const response = await fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=104`);
+      const response = await fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=32`);
       const jsonifiedDrawCardResponse = await response.json();
       if (!response.ok) {
         const errorMessage = `${response.status} ${response.statusText}`;
@@ -27,3 +27,8 @@ export default class DeckOfCardsApi {
     }
   }
 }
+
+//TODO Bug is caused by the thing getting stuck on if condition adding a if that stops it immediately if cards are the same
+//TODO Want to create feature that stops user from clicking card again if card has already been matched probably will be some sort of id selection maybe taken from the SET object
+//TODO Create End Screen for game over condition with play again feature a maybe goal is probably a counter for how many moves taken or time spent an accuracy counter would be neat
+//TODO 
